4cd6958b610f3b1d85bd36b3b6ee6d0170a6808b
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-11-01 18:08:57 +0100

    added a bonus feature: each user's page shows all the gossips that (s)he submitted

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,7 @@
 class UserController < ApplicationController
   def show
-    puts @id = (params[:user_id]).to_i
-    puts @user = User.find_by(id: @id)
+    @id = (params[:user_id]).to_i
+    @user = User.find_by(id: @id)
+    @user_gossips = Gossip.where(user_id: @id)
   end
 end
