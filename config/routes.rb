5ddf75e6145b95d72a2c0275dddb879c14e39be2
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-11-01 12:03:40 +0100

    everything OK, now at 2.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'team/', to: 'team#show'
+  get 'contact/', to: 'contact#show'
 end
