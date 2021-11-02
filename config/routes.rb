2894d1b2bfa71c5bdc8e06dd4ff851a5fbd91e33
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-11-02 10:55:38 +0100

    maj des routes selon la convention REST/CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homepage#show'
-  get 'team/', to: 'team#show'
-  get 'contact/', to: 'contact#show'
+  get 'team/', to: 'team#index'
+  get 'contact/', to: 'contact#index'
   get 'welcome/:first_name', to: 'welcome#show'
   get 'gossip/:gossip_id', to: 'gossip#show', as: 'gossip'
   get 'user/:user_id', to: 'user#show', as: 'user'
